cb89e20bc442313f3de2074102bee119b742f97e
Dzianis Dashkevich <ddashkevich@brightcove.com>
2023-11-12 22:36:16 -0500

    chore: add worspaces scripts

--- happydom.ts
+++
@@ -1,6 +0,0 @@
-import { GlobalRegistrator } from '@happy-dom/global-registrator';
-
-// TODO: console workaround, remove once fixed: https://github.com/oven-sh/bun/issues/6044
-const oldConsole = console;
-GlobalRegistrator.register();
-window.console = oldConsole;
