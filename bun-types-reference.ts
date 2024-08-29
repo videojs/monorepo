d4da95211098388334b8af2089e5f28965ab1a51
Dzianis Dashkevich <ddashkevich@brightcove.com>
2024-08-29 14:40:24 -0400

    remove bun types, lock and build files

--- bun-types-reference.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="bun-types" />
-/// <reference lib="dom" />
--- packages/playback/bun.build.ts
+++
@@ -1,6 +0,0 @@
-import baseBuilder from '../../bun.build.base';
-
-await baseBuilder({
-  entrypoints: ['./src/player.ts', './src/hls-pipeline.ts', './src/dash-pipeline.ts'],
-  outdir: './dist',
-});
