6986fa77aaf913051881d190271e2e3920017927
Dzianis Dashkevich <ddashkevich@brightcove.com>
2024-09-04 22:10:11 -0400

    chore: add clean install script

--- packages/videojs.dev/src/demo.ts
+++ packages/videojs.dev/src/demo.ts
@@ -1,6 +1,7 @@
+/* eslint-disable no-console */
 import { FullPlaylistParser as HlsFullParser, ProgressiveParser as HlsProgressiveParser } from '@videojs/hls-parser';
 import { FullManifestParser as DashFullParser, ProgressiveParser as DashProgressiveParser } from '@videojs/dash-parser';
-import { Player } from '@videojs/playback/player';
+import { Player } from '@videojs/playback';
 
 console.log(HlsFullParser);
 console.log(HlsProgressiveParser);
