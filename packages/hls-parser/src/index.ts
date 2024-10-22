157fc4e5a9d1302994875f2ebd43481839dcec8e
Dzianis Dashkevich <ddashkevich@brightcove.com>
2024-10-22 13:25:11 -0400

    chore: fix hls-parser tests

--- packages/hls-parser/src/index.ts
+++ packages/hls-parser/src/index.ts
@@ -1,4 +1,5 @@
 export { ChunkPlaylistParser } from './lib/parser/chunk-playlist-parser';
+export { FullPlaylistParser } from './lib/parser/full-playlist-parser';
 export * from './lib/types/parsed-playlist';
 export * from './lib/types/parser-options';
 export * from './lib/types/shared-state';
