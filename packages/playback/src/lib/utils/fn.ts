c2f8ecf154244341cd7a53760725e4be1f883869
Dzianis Dashkevich <ddashkevich@brightcove.com>
2024-10-28 17:00:13 -0400

    chore: add entity and async-entity fn utils

--- packages/playback/src/lib/utils/fn.ts
+++ packages/playback/src/lib/utils/fn.ts
@@ -1,3 +1,7 @@
 export const noop = (): void => {};
 
+export const entity = <T>(val: T): T => val;
+
+export const asyncEntity = async <T>(val: T): Promise<T> => val;
+
 export const t = (): boolean => true;
